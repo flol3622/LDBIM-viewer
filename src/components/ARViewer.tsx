f15052b85c9066cc0b12e440a14e0ed2f44bdcea
philippe <philippe.soubrier@gmail.com>
2023-03-07 16:48:53 +0100

    test

---
+++ src/components/ARViewer.tsx
@@ -0,0 +1,23 @@
+import Viewer from "@xeokit/xeokit-sdk/src/viewer/Viewer";
+
+type ARViewerProps = {
+  src?: string;
+  height: string;
+  models?: string[];
+};
+
+export default function ARViewer(props: ARViewerProps) {
+  const myViewer = new Viewer({
+    canvasId: "myCanvas",
+    transparent: true,
+  });
+
+  return (
+    <div
+      className="modelContainer"
+      style={{ width: "100%", height: props.height }}
+    >
+      <canvas id="myCanvas" style={{ width: "100%", height: props.height }} />
+    </div>
+  );
+}
