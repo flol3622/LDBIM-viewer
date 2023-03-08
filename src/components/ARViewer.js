6028c4e63d951fcacf96f3d4ed77b9b2a3ec1ba2
philippe <philippe.soubrier@gmail.com>
2023-03-08 15:25:08 +0100

    function component

--- src/components/ARViewer.js
+++ src/components/ARViewer.js
@@ -2,14 +2,14 @@ import { GLTFLoaderPlugin, NavCubePlugin } from "@xeokit/xeokit-sdk";
 import { Viewer } from "@xeokit/xeokit-sdk/src/viewer/Viewer";
 import { Component } from "react";
 
-export default class ARViewer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+import { useEffect } from "react";
 
-  componentDidMount() {
-    const viewer = this.setViewer();
+export default function ARViewer(props) {
+  useEffect(() => {
+    const viewer = new Viewer({
+      canvasId: "myCanvas",
+      transparent: true,
+    });
 
     const loader = new GLTFLoaderPlugin(viewer);
 
@@ -31,42 +31,27 @@ export default class ARViewer extends Component {
     const scene = viewer.scene;
     const camera = scene.camera;
     camera.prjection = "perspective";
-  }
 
-  setViewer() {
-    const viewer = new Viewer({
-      canvasId: "myCanvas",
-      transparent: true,
-    });
-    return viewer;
-  }
+    return () => {
+      viewer.destroy();
+    };
+  }, []);
 
-  render() {
-    return (
-      <div
-        className="modelContainer"
-        style={{ width: "100%", height: this.props.height }}
-      >
-        <canvas
-          id="myCanvas"
-          style={{ width: "100%", height: this.props.height }}
-        ></canvas>
-        <canvas
-          style={{
-            height: "200px",
-            width: "200px",
-            position: "fixed",
-            right: 0,
-            bottom: 0,
-          }}
-          className="navCube"
-          id="myNavCubeCanvas"
-        ></canvas>
-        <canvas
-          id="mySectionPlanesOverviewCanvas"
-          className="sections"
-        ></canvas>
-      </div>
-    );
-  }
+  return (
+    <div className="modelContainer" style={{ width: "100%", height: props.height }}>
+      <canvas id="myCanvas" style={{ width: "100%", height: props.height }}></canvas>
+      <canvas
+        style={{
+          height: "200px",
+          width: "200px",
+          position: "fixed",
+          right: 0,
+          bottom: 0,
+        }}
+        className="navCube"
+        id="myNavCubeCanvas"
+      ></canvas>
+      <canvas id="mySectionPlanesOverviewCanvas" className="sections"></canvas>
+    </div>
+  );
 }
