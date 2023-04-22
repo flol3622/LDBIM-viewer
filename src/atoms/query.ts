db478cfc807eda7b06f5f64a2e89ffb594a7bb93
philippe <philippe.soubrier@gmail.com>
2023-04-22 15:21:20 +0200

    query

--- src/atoms/query.ts
+++ src/atoms/query.ts
@@ -2,26 +2,23 @@ import { atom } from "recoil";
 
 const uiQuery = atom<string>({
   key: "uiQuery",
-  default: `
-  PREFIX bot: <https://w3id.org/bot#>
-  PREFIX fog: <https://w3id.org/fog#>
-  PREFIX omg: <https://w3id.org/omg#>
-  PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-  PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-  select ?element ?fog_geometry ?geometryData
-  where { 
-      # inst:room_1xS3BCk291UvhgP2dvNvkU bot:containsElement ?element .
-      #	?element omg:hasGeometry ?geometry .
-      #	?geometry ?fog_geometry ?geometryData .
-      ?element ?fog_geometry ?geometryData .
-      FILTER NOT EXISTS { ?element rdf:type bot:Space }
-      FILTER(?fog_geometry IN (fog:asStl)) 
-      FILTER(datatype(?geometryData) = xsd:anyURI)
-  } 
-  #ORDER BY (?element) (?fog_geometry)
-  #LIMIT 20
-    
-  `,
+  default: `PREFIX bot: <https://w3id.org/bot#>
+PREFIX fog: <https://w3id.org/fog#>
+PREFIX omg: <https://w3id.org/omg#>
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+select ?element ?fog_geometry ?geometryData
+where { 
+    # inst:room_1xS3BCk291UvhgP2dvNvkU bot:containsElement ?element .
+    #	?element omg:hasGeometry ?geometry .
+    #	?geometry ?fog_geometry ?geometryData .
+    ?element ?fog_geometry ?geometryData .
+    FILTER NOT EXISTS { ?element rdf:type bot:Space }
+    FILTER(?fog_geometry IN (fog:asStl)) 
+    FILTER(datatype(?geometryData) = xsd:anyURI)
+} 
+#ORDER BY (?element) (?fog_geometry)
+#LIMIT 20`,
 });
 
 export default uiQuery;
--- src/components/Querypannel.tsx
+++ src/components/Querypannel.tsx
@@ -1,4 +1,5 @@
 import { PaperPlaneIcon } from "@radix-ui/react-icons";
+import TextareaAutosize from "@mui/base/TextareaAutosize";
 import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { uiQuery } from "~/atoms";
@@ -11,28 +12,40 @@ export default function Querrypannel() {
     setTempQuery(queryValue);
   }, []);
 
+  const updateQuery = () => {
+    setQuery(tempQuery);
+    console.log("updated query");
+  };
+
+  const handleKeyDown = (e: any) => {
+    if (e.key === "Enter" && e.shiftKey) {
+      updateQuery();
+    }
+  };
+
   return (
-    <>
-      <div className="absolute bottom-4 left-4 flex h-[500px] w-[400px] flex-col rounded border bg-white p-2 pb-10 shadow-lg">
-        <h3>Query</h3>
-        <hr />
-        <textarea
-          spellCheck="false"
-          defaultValue={tempQuery}
-          className="flex-grow p-3 text-xs"
-          onChange={(e) => setTempQuery(e.target.value)}
-          style={{
-            fontFamily: '"Fira code", "Fira Mono", monospace',
-          }}
-        />
-      </div>
-      <PaperPlaneIcon
-        className="absolute bottom-8 left-8 cursor-pointer"
-        onClick={() => {
-          setQuery(tempQuery);
-          console.log("queryValue", queryValue);
+    <div className="absolute bottom-4 left-4 flex w-[400px] flex-col rounded border bg-white p-2 shadow-lg">
+      <h3 className="pb-1">Query</h3>
+      <hr />
+      <TextareaAutosize
+        minRows={4}
+        aria-label="maximum height"
+        placeholder="Write query"
+        defaultValue={tempQuery}
+        spellCheck="false"
+        style={{
+          width: "100%",
+          padding: "0.5rem",
+          fontFamily: '"Fira code", "Fira Mono", monospace',
+          fontSize: "0.7rem",
         }}
+        onChange={(e) => setTempQuery(e.target.value)}
+        onKeyDown={handleKeyDown}
       />
-    </>
+      <div className="my-2 flex w-full items-center justify-between gap-2 px-2 ">
+        <PaperPlaneIcon className="cursor-pointer" onClick={updateQuery} />
+        <p className="text-sm opacity-50">shortcut: Shift + Enter</p>
+      </div>
+    </div>
   );
 }
