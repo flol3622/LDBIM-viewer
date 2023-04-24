d5c5097169c5341226f3e21934d54d0248b0d90f
philippe <philippe.soubrier@gmail.com>
2023-04-24 18:59:26 +0200

    bot location

---
+++ src/modules/BOTlocation/raycasting.ts
@@ -0,0 +1,30 @@
+import { Viewer } from "@xeokit/xeokit-sdk";
+import { LRUMap } from "lru_map";
+
+interface LRUWithFilter extends LRUMap<string, any> {
+  filter(callback: (key: string) => boolean): string[];
+}
+
+function findRoom(viewer: Viewer, position: number[], lru: LRUWithFilter) {
+  const down = [0, 0, -1];
+  const up = [0, 0, 1];
+  const rooms = lru.filter((key) => key.startsWith("room"));
+
+  function pick(direction: number[]) {
+    return viewer.scene.pick({
+      origin: position,
+      direction: direction,
+      pickSurface: true,
+      includeEntities: rooms,
+    });
+  }
+
+  const resultDown = pick(down);
+  const resultUp = pick(up);
+
+  if (resultDown?.entity === resultUp?.entity) {
+    return resultDown?.entity?.id;
+  }
+}
+
+export { findRoom };
